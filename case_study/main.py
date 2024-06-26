07b4cb1eccb738d1fa7fdc7a6f0faf8b6b0d2718
nagakarthik-dot <nagakarthikjetti@gmail.com>
2024-06-26 16:40:23 +0530

    done 1,2,3,4,5,7,10,11,13,14,15,19,27

--- case_study/main.py
+++ case_study/main.py
@@ -31,6 +31,7 @@ def solve_problem(problem):
 
 
 if __name__ == '__main__':
+    print('Done:  [1,2,3,4,5,7,10,11,13,14,15,19,27]')
     problem_choice = input("Enter Problem num: ")
     
     solve_problem(int(problem_choice))
