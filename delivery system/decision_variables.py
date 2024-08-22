588f8ee65c2a702f98d6cb571f4b2d2df1e8952c
nagakarthik-dot <nagakarthikjetti@gmail.com>
2024-08-22 11:18:39 +0530

    done

--- delivery system/decision_variables.py
+++ delivery system/decision_variables.py
@@ -7,20 +7,19 @@ class DecisionVariables:
         self.data=data
         self.trucks=None
         self.select=None
-        self.min_10=None
-        self.min_15=None
+        self.min_trucks=None
+        #self.min_15=None
         
 
     def create_variables(self):
         self.trucks = create_trucks_vars(self.model,self.data)
         self.select = create_select_vars(self.model,self.data)
-        self.min_10 = create_min_10_trucks_vars(self.model,self.data)
-        self.min_15 = create_min_10_trucks_vars(self.model,self.data)
+        self.min_trucks = create_min_trucks_vars(self.model,self.data)
+        #self.min_15 = create_min_10_trucks_vars(self.model,self.data)
 
     def get_variables(self):
         return {
             'select':self.select,
             'trucks':self.trucks,
-            'min_10': self.min_10,
-            'min_15':self.min_15
+            'min_trucks': self.min_trucks
         }
